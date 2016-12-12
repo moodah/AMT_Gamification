9a91ab919f5f4bce5663f2cc060fa70a5a547040
chlablak <chlablak@gmail.com>
2016-12-12 15:44:25 +0100

    tests with mocha working (infra)

---
+++ tests/images/mocha/test/application.js
@@ -0,0 +1,11 @@
+// Testing the '/application' endpoint
+
+// This test is just for testing the "workfullness" of mocha
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
