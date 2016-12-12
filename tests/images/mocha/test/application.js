6a4c928e9766865a1f003f68acf91e189189df98
chlablak <chlablak@gmail.com>
2016-12-12 16:23:16 +0100

    tests: add chai and sinon

--- tests/images/mocha/test/application.js
+++ tests/images/mocha/test/application.js
@@ -1,11 +1,11 @@
 // Testing the '/application' endpoint
 
-// This test is just for testing the "workfullness" of mocha
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var chai = require('chai');
+var assert = chai.assert;
+var should = chai.should();
+
+describe('/application endpoint', function () {
+    it('should ...', function (done) {
+        done();
     });
-  });
 });
