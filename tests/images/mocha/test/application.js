d78e7dac7c3de4b8fce8569a100b60466eb8c8a5
chlablak <chlablak@gmail.com>
2016-12-12 17:03:54 +0100

    tests: spec for /applications endpoints (not impl.)

--- tests/images/mocha/test/application.js
+++ tests/images/mocha/test/application.js
@@ -1,11 +1,27 @@
-// Testing the '/application' endpoint
+// Testing the '/applications' endpoint:
 
 var chai = require('chai');
 var assert = chai.assert;
 var should = chai.should();
 
-describe('/application endpoint', function () {
-    it('should ...', function (done) {
-        done();
+describe('/applications endpoint', function () {
+
+    it('should allow to register a new application', function (done) {
+        
+    });
+
+    it('should not allow to register two applications with the same name', function (done) {
+        
+    });
+
+    describe('/applications/auth endpoint', function () {
+        
+        it('should returns the token for the application', function (done) {
+            
+        });
+
+        it('should returns an error with bad credentials', function (done) {
+            
+        });
     });
 });
