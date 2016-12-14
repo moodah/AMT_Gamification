1bdc16f22973d78206a23f049d538337ab1f553f
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2016-12-14 16:48:04 +0100

    Connection to MySQL database (for now mysql running apart in a bash, not in docker)

--- platform/src/main/java/ch/heigvd/amt/gamification/api/ApplicationsApiController.java
+++ platform/src/main/java/ch/heigvd/amt/gamification/api/ApplicationsApiController.java
@@ -9,12 +9,7 @@ import io.swagger.annotations.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -22,7 +17,7 @@ import java.util.List;
 
 @javax.annotation.Generated(value = "class ch.heigvd.amt.gamification.codegen.languages.SpringCodegen", date = "2016-12-13T18:36:02.067Z")
 
-@Controller
+@RestController
 public class ApplicationsApiController implements ApplicationsApi {
 
     public ResponseEntity<Token> applicationsAuthPost(@ApiParam(value = "The application informations" ,required=true ) @RequestBody Application application) {
