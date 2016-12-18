894af90fa7116c5c00976adbc55fadb03a75e4fa
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2016-12-18 16:03:24 +0100

    Added tests on applications (not impl)

--- platform/src/main/java/ch/heigvd/amt/gamification/configuration/AppConfig.java
+++ platform/src/main/java/ch/heigvd/amt/gamification/configuration/AppConfig.java
@@ -8,7 +8,7 @@ package ch.heigvd.amt.gamification.configuration;
 public class AppConfig {
 
     public static final String X_GAMIFICATION_TOKEN = "Authorization";
-    public static final int MIN_APP_NAME_LENGTH = 2;
+    public static final int MIN_APP_NAME_LENGTH = 1;
     public static final int MAX_APP_NAME_LENGTH = 128; // TODO : nécessaire ?
     public static final int MIN_APP_PWD_LENGTH = 8;
     public static final int MAX_APP_PWD_LENGTH = 128; // TODO : nécessaire ?
