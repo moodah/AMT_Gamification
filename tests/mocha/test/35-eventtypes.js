b5cec1fde997645a6d2823ab39a6a5a7d83fac84
chlablak <chlablak@gmail.com>
2017-01-24 15:54:22 +0100

    tests: replace rules/ with 3 endpoints (no spec atm)

---
+++ tests/mocha/test/35-eventtypes.js
@@ -0,0 +1,14 @@
+// Testing eventtypes/
+// -------------------
+
+// Utils
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let CONFIG = require('./config');
+let shared = require('./shared');
+let Utils = require('./utils');
+
+describe('eventtypes/', function () {
+
+});
---
+++ tests/mocha/test/45-badgeachievements.js
@@ -0,0 +1,14 @@
+// Testing badgeachievements/
+// --------------------------
+
+// Utils
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let CONFIG = require('./config');
+let shared = require('./shared');
+let Utils = require('./utils');
+
+describe('badgeachievements/', function () {
+    
+});
