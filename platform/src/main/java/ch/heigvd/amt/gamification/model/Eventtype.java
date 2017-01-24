be1379a439343b335a37119cee42933d290aea1f
Moodah <christopher.browne@heig-vd.ch>
2017-01-24 11:10:33 +0100

    Update Eventtype
    Rollback edit, must be done in swagger

--- platform/src/main/java/ch/heigvd/amt/gamification/model/Eventtype.java
+++ platform/src/main/java/ch/heigvd/amt/gamification/model/Eventtype.java
@@ -31,6 +31,9 @@ public class Eventtype   {
     private String name;
 
     @Column(nullable = false)
+    private BigDecimal code;
+
+    @Column(nullable = false)
     private BigDecimal points;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "eventtype")
