07ba2852dc582e0b7d32e9b98172dd5775669b35
Moodah <christopher.browne@heig-vd.ch>
2017-01-25 14:34:08 +0100

    Respond 404 on NullPointerException everywhere

--- platform/src/main/java/ch/heigvd/amt/gamification/api/UsersApi.java
+++ platform/src/main/java/ch/heigvd/amt/gamification/api/UsersApi.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @javax.annotation.Generated(value = "class ch.heigvd.amt.gamification.codegen.languages.SpringCodegen", date = "2016-12-18T13:30:19.867Z")
 
@@ -22,9 +23,7 @@ public interface UsersApi {
             produces = {"application/json"},
             consumes = {"application/json"},
             method = RequestMethod.GET)
-    ResponseEntity<ArrayList<UserPresentationDTO>> usersGet(@ApiParam(value = "Application token", required = true) @RequestHeader(value = "Authorization", required = true) String authorization,
-                                                            @ApiParam(value = "The page number", defaultValue = "1") @RequestParam(value = "page", required = false, defaultValue = "1") BigDecimal page,
-                                                            @ApiParam(value = "Number of result per page", defaultValue = "10") @RequestParam(value = "perPage", required = false, defaultValue = "10") BigDecimal perPage);
+    ResponseEntity<List<UserPresentationDTO>> usersGet(@ApiParam(value = "Application token", required = true) @RequestHeader(value = "Authorization", required = true) String authorization);
 
 
     @ApiOperation(value = "Get user with specified id", notes = "", response = UserPresentationDTO.class, tags = {"Users",})
